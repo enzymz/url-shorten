6353d82abc196aa7b032afc7d853c745eb2b7ac1
enzymz <tranxuanha.ptit@gmail.com>
2016-12-08 19:56:04 +0700

    fix mail setting

--- xurl/settings/base.py
+++ xurl/settings/base.py
@@ -114,8 +114,8 @@ GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
 
 # Email
 EMAIL_USE_TLS=True
-EMAIL_HOST='smpt.gmail.com'
+EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER=os.environ.get('ADMIN_MAIL')
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT=587
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
