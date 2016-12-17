f2adb54b0b49b7f7f28621185c2194b54376a7d3
enzymz <tranxuanha.ptit@gmail.com>
2016-12-17 17:43:10 +0700

    chart

--- xurl/views.py
+++ xurl/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
-
+from django.contrib.sites.models import Site
+from shorturl.views import get_domain
+from django.http import HttpResponse
+from django.http import HttpResponse
 
 class ChartForm(TemplateView):
     template_name = 'dash/dashboard.html'
-
-
